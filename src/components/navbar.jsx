4546cb74d484f8e6a5b30571b762d4aeb6ff14fa
yasi <zahrafarrokhi2017@gmail.com>
2022-06-17 16:03:21 +0430

    link

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,11 +1,13 @@
+import {Link, NavLink} from 'react-router-dom';
+
 const Navbar = () => {
   return (
     <>
       <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <div className="container-fluid">
-          <a className="navbar-brand" href="/">
+          <Link className="navbar-brand" to="/">
             Home
-          </a>
+          </Link>
           <button
             className="navbar-toggler"
             type="button"
@@ -20,19 +22,19 @@ const Navbar = () => {
           <div className="collapse navbar-collapse" id="navbarNav">
             <ul className="navbar-nav">
               <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="/users">
+                <NavLink className="nav-link" aria-current="page" to="/users">
                   Users
-                </a>
+                </NavLink>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="/login">
+                <NavLink className="nav-link" to="/login">
                   Login
-                </a>
+                </NavLink>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="/register">
+                <NavLink className="nav-link" to="/register">
                   Register
-                </a>
+                </NavLink>
               </li>
             </ul>
           </div>
--- src/index.js
+++ src/index.js
@@ -1,10 +1,7 @@
 import ReactDOM from "react-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router,} from "react-router-dom";
 
-import {
-  BrowserRouter as Router,
- 
-} from "react-router-dom";
 import App from "./app";
 
 ReactDOM.render(
