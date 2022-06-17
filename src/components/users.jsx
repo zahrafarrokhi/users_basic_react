1ec7e0b85756e45fb5d691af3c20ab3dc6c72adb
yasi <zahrafarrokhi2017@gmail.com>
2022-06-17 12:39:13 +0430

    add basic files

---
+++ src/components/users.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import axios from "axios";
+
+class Users
+ extends Component {
+  state = {  } 
+  render() { 
+    return (
+      <div>users</div>
+    );
+  }
+}
+ 
+export default Users;
