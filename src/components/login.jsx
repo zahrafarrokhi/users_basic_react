b1aa21205934a17a5e7e6dd939a80cacfa6cff54
yasi <zahrafarrokhi2017@gmail.com>
2022-06-19 21:38:11 +0430

    add component

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 // import axios from 'axios';
+import Input from './input';
 
 class Login extends Component {
   state ={
@@ -23,15 +24,9 @@ class Login extends Component {
     const {email,password} = this.state.account;
     return (
     <form onSubmit={this.handleSubmit}>
-      <div className="mb-3">
-        <label htmlFor="email">Email:</label>
-        <input onChange={this.handleChange} value={email}  id="email" name='email' className='form-control' type="text"/>
-      </div>
-      <div className="mb-3">
-        <label htmlFor="password">Password:</label>
-        <input onChange={this.handleChange} value={password}  id="password" name='password' className='form-control' type="text"/>
-      </div>
-      <button className="btn btn-primary">Login</button>
+      <Input name='email' value={email} label='Email' onChange={this.handleChange} />
+      <Input name='password' value={password} label='Password' onChange={this.handleChange} />
+           <button className="btn btn-primary">Login</button>
     </form>)
   }
 }
