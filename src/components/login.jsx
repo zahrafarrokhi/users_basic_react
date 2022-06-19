6b302152340015458589d575f52b5fecd336b923
yasi <zahrafarrokhi2017@gmail.com>
2022-06-19 20:24:56 +0430

    add form

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -1,8 +1,23 @@
 import React, { Component } from 'react';
 
 class Login extends Component {
+  handleSubmit = (e)=>{
+    e.preventDefault();
+    console.log('submitted')
+  }
   render() { 
-    return <h1>Login</h1>;
+    return (
+    <form onSubmit={this.handleSubmit}>
+      <div className="mb-3">
+        <label htmlFor="email">Email:</label>
+        <input id="email" className='form-control' type="text"/>
+      </div>
+      <div className="mb-3">
+        <label htmlFor="password">Password:</label>
+        <input id="password" className='form-control' type="text"/>
+      </div>
+      <button className="btn btn-primary">Login</button>
+    </form>)
   }
 }
  
